d8b8f6d24d14df97c7ab1d9e137aba60c61a8e5c
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-13 09:46:10 +0600

    Update IncomeTaxType.java

--- inheritance (lesson 5)/TAX/IncomeTaxType.java
+++ inheritance (lesson 5)/TAX/IncomeTaxType.java
@@ -4,8 +4,6 @@ public class IncomeTaxType extends TaxType {
     @Override
     public double calculateTaxFor(double amount) {
         double incomeTaxType = amount * 0.13;
-
         return incomeTaxType;
     }
-
 }
