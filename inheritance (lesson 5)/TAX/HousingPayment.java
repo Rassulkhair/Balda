8b236d8b8a113fc132da650bfeee5e81fe0da7f6
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-13 09:45:54 +0600

    Update HousingPayment.java

--- inheritance (lesson 5)/TAX/HousingPayment.java
+++ inheritance (lesson 5)/TAX/HousingPayment.java
@@ -1,11 +1,10 @@
 package TAX;
 
 public class HousingPayment extends Bill {
-
     public HousingPayment(double amount, TaxType taxType, TaxService taxService) {
         super(amount, taxType, taxService);
     }
-
+    
     @Override
     public void payTaxes() {
         super.payTaxes();
