1bd12ef39aa1f9b745333b711f061352c4564c61
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-13 09:45:29 +0600

    Update Bill.java

--- inheritance (lesson 5)/TAX/Bill.java
+++ inheritance (lesson 5)/TAX/Bill.java
@@ -4,20 +4,15 @@ public class Bill {
     private double amount;
     private TaxType taxType;
     private TaxService taxService;
-
+    
     public Bill(double amount, TaxType taxType, TaxService taxService){
         this.amount=amount;
         this.taxType=taxType;
         this.taxService=taxService;
     }
-
+    
     public void payTaxes(){
-        //TODO
-
         double taxAmount=1.1;
-
         taxService.payOut(taxAmount);
     }
-
-
 }
