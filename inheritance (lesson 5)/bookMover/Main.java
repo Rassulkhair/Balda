1a0d0f27dfe64a2dd27dc425519a32a1b166d877
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-13 09:49:26 +0600

    Update Main.java

--- inheritance (lesson 5)/bookMover/Main.java
+++ inheritance (lesson 5)/bookMover/Main.java
@@ -5,8 +5,5 @@ public class Main {
         Book book = new Book("The Lord of the Rings" );
         BookMover fromAvailableStatusMover = new FromAvailableBookMover();
         fromAvailableStatusMover.moveToStatus(book, Status.ARCHIVED);
-
-
-
     }
 }
