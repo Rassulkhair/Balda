b86e882bb8f88078bf9c62620ff70d606ac97871
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-13 09:47:36 +0600

    Update Book.java

--- inheritance (lesson 5)/bookMover/Book.java
+++ inheritance (lesson 5)/bookMover/Book.java
@@ -3,19 +3,16 @@ package bookMover;
 public class Book {
     private String title;
     private String status;
-
+    
     public Book(String title) {
         this.title = title;
     }
-
     public String getTitle() {
         return title;
     }
-
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
