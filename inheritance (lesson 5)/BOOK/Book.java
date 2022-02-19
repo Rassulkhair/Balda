e1203f2e21d38b68dda90dbbac7b3e03e7f0e38b
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-19 09:28:44 +0600

    Update Book.java

--- inheritance (lesson 5)/BOOK/Book.java
+++ inheritance (lesson 5)/BOOK/Book.java
@@ -1,22 +1,19 @@
 package BOOK;
 
 public class Book {
+    
     private String title;
+    
     private String status;
 
     public Book(String title) {
         this.title = title;
+        this.status=Status.AVAILABLE;
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title;}
 
-    public String getStatus() {
-        return status;
-    }
+    public String getStatus() {return status; }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    public void setStatus(String status) { this.status = status;}
 }
