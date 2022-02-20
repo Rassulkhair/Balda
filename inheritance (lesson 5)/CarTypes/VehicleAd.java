ab1cef761489cc39e2622805b6e308afce0a30f2
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-20 09:46:09 +0600

    Update VehicleAd.java

--- inheritance (lesson 5)/CarTypes/VehicleAd.java
+++ inheritance (lesson 5)/CarTypes/VehicleAd.java
@@ -7,8 +7,13 @@ public class VehicleAd {
     private VehicleTypeByBodyTypes vehicleTypeByBodyTypes;
     private VehicleTypeByFuelTypes vehicleTypeByFuelTypes;
 
-    public VehicleAd(String model, String id, VehicleTypeByPurpose vehicleTypeByPurpose,
-                     VehicleTypeByBodyTypes vehicleTypeByBodyTypes, VehicleTypeByFuelTypes vehicleTypeByFuelTypes) {
+    public VehicleAd(
+        String model,
+        String id,
+        VehicleTypeByPurpose vehicleTypeByPurpose,
+        VehicleTypeByBodyTypes vehicleTypeByBodyTypes,
+        VehicleTypeByFuelTypes vehicleTypeByFuelTypes
+    ) {
         this.model = model;
         this.id = id;
         this.vehicleTypeByPurpose = vehicleTypeByPurpose;
@@ -16,32 +21,18 @@ public class VehicleAd {
         this.vehicleTypeByFuelTypes = vehicleTypeByFuelTypes;
     }
 
-    //for creating new Car
-    public VehicleAd(String model) {
-        this.model = model;
+    public VehicleAd(String model) {this.model = model;
     }
 
-    public VehicleTypeByPurpose getVehicleTypeByPurpose() {
-        return vehicleTypeByPurpose;
-    }
+    public VehicleTypeByPurpose getVehicleTypeByPurpose() { return vehicleTypeByPurpose;}
 
-    public VehicleTypeByBodyTypes getVehicleTypeByBodyTypes() {
-        return vehicleTypeByBodyTypes;
-    }
+    public VehicleTypeByBodyTypes getVehicleTypeByBodyTypes() { return vehicleTypeByBodyTypes; }
 
-    public VehicleTypeByFuelTypes getVehicleTypeByFuelTypes() {
-        return vehicleTypeByFuelTypes;
-    }
+    public VehicleTypeByFuelTypes getVehicleTypeByFuelTypes() { return vehicleTypeByFuelTypes; }
 
-    public String getModel() {
-        return model;
-    }
+    public String getModel() {return model;}
 
-    public String getId() {
-        return id;
-    }
+    public String getId() {return id; }
 
-    public String toString() {
-        return this.model;
-    }
+    public String toString() {return this.model; }
 }
