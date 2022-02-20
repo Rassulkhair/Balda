e4130a4838ab584c5f2082db2256035fd9eb7e0f
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-20 09:54:31 +0600

    Update Main.java

--- inheritance (lesson 5)/CarTypes/Main.java
+++ inheritance (lesson 5)/CarTypes/Main.java
@@ -2,32 +2,17 @@ package CarTypes;
 
 public class Main {
     public static void main(String[] args) {
+        
         AdsService adsService = new AdsService();
-        VehicleAd volvoAd = new VehicleAd("Volvo", "123", new PassengerType(),
-                new SedanType(), new PetrolType());
-        VehicleAd kamazAd = new VehicleAd("Kamaz", "45", new TruckType(),
-                new PickupType(), new DieselType());
+        VehicleAd ad1 = new VehicleAd("Volvo", "123", new PassengerType(), new SedanType(), new PetrolType());
+        VehicleAd ad2 = new VehicleAd("Kamaz", "45", new TruckType(),new PickupType(), new DieselType());
+        Vehivle Ad ad3 = new VehicleAd(77, new CarType(), new SedanType(), new DieselType()); 
 
-        adsService.setAdList(new VehicleAd[]{volvoAd, kamazAd});
-
-        adsService.filterByVehicleTypeByPurpose(new PassengerType());
-
-        adsService.filterByVehicleTypeByPurpose(new TruckType());
-
-        //TODO Создайте объявление с типами CAR, SEDAN, PETROL и отфильтруйте объявления с бензиновым топливом
-
-        VehicleAd chevroletAd = new VehicleAd("chevrolet", "666", new PassengerType(),
-                new SedanType(), new PetrolType());
-
-        VehicleAd bugattiAd = new VehicleAd("bugatti", "873", new PassengerType(),
-                new SedanType(), new PetrolType());
-
-        adsService.setAdList(new VehicleAd[]{chevroletAd, bugattiAd});
+        adsService.setAdList(new VehicleAd[]
+                             {ad1, ad2,ad3});
 
         adsService.filterByVehicleTypeByPurpose(new PassengerType());
-
         adsService.filterByVehicleTypeByPurpose(new TruckType());
-
-
+        adsService.filterByVehicleTypeByPurpose(new PetrolType());
     }
 }
