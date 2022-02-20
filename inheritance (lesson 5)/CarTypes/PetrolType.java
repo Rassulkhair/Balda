2e178f3437d3af27d4ce79ac3500037d194f6727
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-20 09:59:11 +0600

    Update PetrolType.java

--- inheritance (lesson 5)/CarTypes/PetrolType.java
+++ inheritance (lesson 5)/CarTypes/PetrolType.java
@@ -1,11 +1,8 @@
 package CarTypes;
 
 public class PetrolType extends VehicleTypeByFuelTypes {
-
-
     @Override
     public String getTypeName() {
         return VehicleTypeEnum.PETROL.name();
-
     }
 }
