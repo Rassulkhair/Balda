33151ba33274e9cd50354c1655d9003ffcc8332a
Rassulkhair <90597917+Rassulkhair@users.noreply.github.com>
2022-02-27 09:15:08 +0600

    Update Main.java

--- inheritance (lesson 5)/GameEpta/Main.java
+++ inheritance (lesson 5)/GameEpta/Main.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int slot = scanner.nextInt();
         Player player = new Player();
        System.out.format("У игрока %d слотов с оружием, "
                 + " ведите номер, чтобы выстрелить "
                 + ",-1 чтобы выйти%n", player.getSlotsCount());
-
-        int slot = scanner.nextInt();
 
         while (slot != -1) {
             player.shotWithWeapons(slot);
